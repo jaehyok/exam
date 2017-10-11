923e87c619fbedb0c72b5597d42a6241a5302f3a
jh <jh@infobank.net>
2017-10-12 01:09:51 +0900

    let's play #3

--- src/exem/Main.java
+++ src/exem/Main.java
@@ -2,6 +2,7 @@ package exem;
 
 import Codingdojang.CD365;
 import Codingdojang.CD393;
+import Codingdojang.CD408;
 
 public class Main {
 
@@ -10,8 +11,11 @@ public class Main {
 	cd365.run();
 	
 	CD393 cd393 = new CD393();
-	long start = System.currentTimeMillis();
 	cd393.run();
+	
+	long start = System.currentTimeMillis();
+	CD408 cd408 = new CD408();
+	cd408.run();
 	long end = System.currentTimeMillis();
 	System.out.println("dt : " + (end - start) / 1000.0);
     }
